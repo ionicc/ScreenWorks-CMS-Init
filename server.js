b0ffcd8409e1f2f9b001afd31e8c7947b3d495ca
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-13 16:07:35 +0530

    Added server.js
    Added server.js.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//Initialising the server --Sagar
+
+//Zzz, Dun remember how to initialse on the environment's port
+//I'll do it in the later commits
+
+app.listen(3000, () => {
+  console.log("Server listening on 3000");
+});
