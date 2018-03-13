bb73a257a6762a4026b1bf564cdc48a22f55bf19
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-13 22:29:41 +0530

    Fixed a typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 //Initialising the server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
   console.log('Server listening on 3000 ${PORT}');
