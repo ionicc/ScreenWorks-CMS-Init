a1e5caa1a8415e3343786277f38f0f65e2909c6e
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-15 23:47:29 +0530

    Adding MongoDB connection and fixing a bug

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/TestDB';
+
 //Initialising the server
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-  console.log('Server listening on 3000 ${PORT}');
+  console.log(`Server listening on ${PORT}`);
 });
 
 //Handling the root get req.
 app.get('/',(req,res,next) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+MongoClient.connect(url, (err,db) => {
+  console.log("Connected to MongoDB Instance");
 });
