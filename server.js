e51b0610414d8fda84feb2314279986b6ed92076
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-16 21:26:23 +0530

    Admin panel and aws server
    Links can now be added from the admin panel to the AWS DB.

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost/TestDB';
+var url = 'mongodb://sagar:aakash@ds115749.mlab.com:15749/screenworks-test';
+
+var dbIns = false;
+
+var dbb;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Initialising the server
 const PORT = process.env.PORT || 3000;
-
-app.listen(3000, () => {
-  console.log(`Server listening on ${PORT}`);
-});
 
 //Handling the root get req.
 app.get('/',(req,res,next) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-MongoClient.connect(url, (err,db) => {
-  console.log("Connected to MongoDB Instance");
+MongoClient.connect(url, (err,client) => {
+  console.log(`Connected to ${url}`);
+  if(err) { console.log(err); }
+
+  db = client.db('screenworks-test');
+
+  app.listen(3000, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+});
+
+app.post('/links', (req,res) => {
+  db.collection('links').save(req.body,(err,result) => {
+    if(err) console.log(err);
+
+    console.log(`Link ${req.body.link} added`);
+    res.redirect('/');
+  })
 });
