b284c0705fba434b3dc212ddace5b78783696613
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-17 20:30:10 +0530

    Base version ready
    Features added:
    ->Added a veiw engine to render the images
    //Images are now being shown in an array as their links are being inserted

--- server.js
+++ server.js
@@ -7,16 +7,24 @@ var url = 'mongodb://sagar:aakash@ds115749.mlab.com:15749/screenworks-test';
 
 var dbIns = false;
 
-var dbb;
+var db;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Initialising the server
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine','ejs');
+
 //Handling the root get req.
 app.get('/',(req,res,next) => {
-  res.sendFile(__dirname + '/index.html');
+//res.sendFile(__dirname + '/index.html');
+  db.collection('links').find().toArray(function(err,results) {
+    console.log(results);
+    res.render('index.ejs',{links:results});
+
+  });
+
 });
 
 MongoClient.connect(url, (err,client) => {
